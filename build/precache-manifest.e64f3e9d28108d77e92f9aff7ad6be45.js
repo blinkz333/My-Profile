93dd12f3f4e7c9f9fede93fc72bae688c2a8e72d
blink <threerapon.r@trendvg3.com>
2020-06-13 22:40:56 +0700

    add

---
+++ build/precache-manifest.e64f3e9d28108d77e92f9aff7ad6be45.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af9d61c8b901be72dfcf7706afe5782",
+    "url": "/My-Profile/index.html"
+  },
+  {
+    "revision": "6d1869276fddf83d0ac8",
+    "url": "/My-Profile/static/css/main.2a360a94.chunk.css"
+  },
+  {
+    "revision": "d46f7758804bd9592fe1",
+    "url": "/My-Profile/static/js/2.9c884e81.chunk.js"
+  },
+  {
+    "revision": "6d1869276fddf83d0ac8",
+    "url": "/My-Profile/static/js/main.4bc50fac.chunk.js"
+  },
+  {
+    "revision": "d55af7f1ed2edf059c59",
+    "url": "/My-Profile/static/js/runtime~main.c96017eb.js"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/My-Profile/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "10314973fddcb0a3aa2519fafa5255e0",
+    "url": "/My-Profile/static/media/blink.10314973.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/My-Profile/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/My-Profile/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/My-Profile/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/My-Profile/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
